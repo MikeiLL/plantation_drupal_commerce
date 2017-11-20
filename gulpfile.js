1687c885f67dfbacfd057b06450f15ac6ee81e14
toptomato <tom@lexwebdev.com>
2017-11-19 19:25:01 -0500

    adding toms sass and gulp files

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Gulpfile
+
+var gulp = require('gulp');
+
+var gutil = require('gulp-util');
+
+gulp.task('log', function(){
+	gutil.log('==My Log Task==')
+});
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('styles/main.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('css'))
+});
+
+var uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
+
+gulp.task('watch', function() {
+	gulp.watch('styles/*/*.scss', ['sass']);
+	gulp.watch('styles/main.scss', ['sass']);
+});
+
+
+
