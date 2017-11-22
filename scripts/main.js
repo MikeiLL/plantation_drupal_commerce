d4b461100aa4849988adf1a2f5fcc539eb8c25ad
Mike iLL <mike@mzoo.org>
2017-11-22 16:37:04 -0600

    Initial commit on server.

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+
+console.log("Hello World from Main Javascript");
+
+console.log('Upload script modified');
+
+document.addEventListener("DOMContentLoaded", function(){
+
+function clean(node)
+{
+  for(var n = 0; n < node.childNodes.length; n ++)
+  {
+    var child = node.childNodes[n];
+    if
+    (
+      child.nodeType === 8 
+      || 
+      (child.nodeType === 3 && !/\S/.test(child.nodeValue))
+    )
+    {
+      node.removeChild(child);
+      n --;
+    }
+    else if(child.nodeType === 1)
+    {
+      clean(child);
+    }
+  }
+}
+
+clean(document.body);
+
+
+});
